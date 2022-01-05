d39390cb4feea1ff02b1bdd8dc19f901500dcf3d
Anna Seregina <anna_s_80@icloud.com>
2022-01-05 10:46:24 -0800

    Address PR comments

--- AdventOfCode2021/day1_1/day1_1.py
+++ AdventOfCode2021/day1_1/day1_1.py
@@ -3,19 +3,18 @@
 from typing import List
 
 def file_read(file_name) -> List[int]:
-    file_data_list: List[int] = []
+    result: List[int] = []
     with open(file_name) as f:
         for line in f:
-            file_data_list.append(int(line))
-    return file_data_list
+            result.append(int(line))
+    return result
 
 
 def main():
-    data_list: List[int] = file_read("day1_1.txt")
-    #data_list: List[int] = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+    input: List[int] = file_read("day1_1.txt")
     increases_count = 0
-    for i in range(1, len(data_list)):
-        if (data_list[i] > data_list[i-1]):
+    for i in range(1, len(input)):
+        if (input[i] > input[i-1]):
             increases_count += 1
     print(f"increases_count = {increases_count}")
     
