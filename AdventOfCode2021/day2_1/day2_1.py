0ef611eef23faaa520c8c395d6013e4b3b8f8c64
AnaSeregina <61445626+AnaSeregina@users.noreply.github.com>
2022-01-06 19:01:47 -0800

    Advent of Code. Day 2. Task 1. Solve the puzzle (#5)
    * Advent of Code. Day 2. Task 1. Solve the puzzle
    
    * Address PR comments
    
    * Make changes: use return instead of break for the for-loop

---
+++ AdventOfCode2021/day2_1/day2_1.py
@@ -0,0 +1,35 @@
+#!python3
+
+from typing import List
+
+def file_read(file_name) -> List[str]:
+    result: List[str] = []
+    with open(file_name) as f:
+        for line in f:
+            result.append(line)
+    return result
+
+
+def main():
+    input: List[str] = file_read("day2_1.txt")
+    position_data_list: List[str] = []
+    pos_horizontal = 0
+    pos_vertical = 0
+    
+    for i in range(0, len(input)):
+        position_data_list = input[i].split(' ')
+        if ('forward' in position_data_list[0]):
+            pos_horizontal += int(position_data_list[1])
+        elif ('down' in position_data_list[0]):
+            pos_vertical += int(position_data_list[1])
+        elif ('up' in position_data_list[0]):
+            pos_vertical -= int(position_data_list[1])
+        else:
+            print("Something went wrong!")
+            return
+    result = pos_horizontal * pos_vertical
+    print(f"result = {result}  pos_horizontal = {pos_horizontal}  pos_vertical = {pos_vertical}")
+    
+
+if __name__ == "__main__":
+    main()
