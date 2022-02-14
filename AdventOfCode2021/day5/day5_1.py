848d6bee5848481403702edeb6ebfc0dec236e29
AnaSeregina <61445626+AnaSeregina@users.noreply.github.com>
2022-02-13 20:19:52 -0800

    Day5 1 (#11)
    * Read input from file. Creat object Line
    
    * Read input from file. Creat object Line
    
    * Read input from file. Creat object Line
    
    * AdventOfCode day 5 task 1. Finish the task.
    
    * change tuple to Tuple

---
+++ AdventOfCode2021/day5/day5_1.py
@@ -0,0 +1,43 @@
+#!python3
+
+from line import Line
+from typing import Dict, List, Tuple
+import os
+
+
+def file_read(file_name) -> List[Line]:
+    lines: List[Line] = []
+    with open(file_name) as f:
+        for line in f:
+            points = line.strip().split(" -> ")
+            l: Line = Line(points[0], points[1])
+            if (l.horizontal() or l.vertical()):
+                lines.append(l)
+    return lines
+
+
+def points_number_of_two_overlaps(lines: List[Line]) -> int:
+    point_overlaps_number: Dict[Tuple[int, int], int] = {}
+    for i in range(len(lines)):
+        line_points: List[Tuple[int, int]] = lines[i].get_all_points()
+        for point in line_points:
+            counter = 1
+            if point in point_overlaps_number:
+                counter += point_overlaps_number.get(point)
+            point_overlaps_number[point] = counter
+
+    points_number = 0
+    for overlap_number in point_overlaps_number.values():
+        if overlap_number >= 2:
+            points_number += 1
+    return points_number
+
+
+def main():
+    lines: List[Line] = file_read("AdventOfCode2021/day5/day5.txt")
+    points_number: int = points_number_of_two_overlaps(lines)
+    print(f"At how many points do at least two lines overlap?\nPoints number: {points_number}")
+    
+
+if __name__ == "__main__":
+    main()
---
+++ AdventOfCode2021/day5/line.py
@@ -0,0 +1,40 @@
+
+from typing import List, Tuple
+from xmlrpc.client import boolean
+
+
+class Line:
+    def __init__(self, start_point: str, end_point: str) -> None:
+        self.start_point = list(map(int, start_point.split(",")))
+        self.end_point = list(map(int, end_point.split(",")))
+        self.x_start = self.start_point[0]
+        self.y_start = self.start_point[1]
+        self.x_end = self.end_point[0]
+        self.y_end = self.end_point[1]
+
+
+    def horizontal(self) -> boolean:
+        return (self.y_start == self.y_end)
+        
+
+    def vertical(self) -> boolean:
+        return (self.x_start == self.x_end)
+
+
+    def get_all_points(self) -> List[Tuple[int, int]]:
+        line_points: List[Tuple[int, int]] = []
+        if (self.horizontal()):
+            point0 = min(self.x_start, self.x_end)
+            point1 = max(self.x_start, self.x_end)
+            for i in range(point0, point1 + 1):
+                line_point: Tuple[int, int] = (i, self.y_start)
+                line_points.append(line_point)            
+        elif (self.vertical()):
+            point0 = min(self.y_start, self.y_end)
+            point1 = max(self.y_start, self.y_end)
+            for i in range(point0, point1 + 1):
+                line_point: Tuple[int, int] = (self.x_start, i)
+                line_points.append(line_point)
+        else:
+            print(f"Diagonal?")
+        return line_points        
