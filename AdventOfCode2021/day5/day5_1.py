cca365daf3d63d76d4a069a61f7a284f036b81ee
Anna Seregina <anna_s_80@icloud.com>
2022-02-14 20:21:50 -0800

    Add some changes for Line class

--- AdventOfCode2021/day5/day5_1.py
+++ AdventOfCode2021/day5/day5_1.py
@@ -1,37 +1,45 @@
 #!python3
 
 from line import Line
+from point import Point
 from typing import Dict, List, Tuple
-import os
 
 
 def file_read(file_name) -> List[Line]:
     lines: List[Line] = []
     with open(file_name) as f:
         for line in f:
-            points = line.strip().split(" -> ")
-            l: Line = Line(points[0], points[1])
+            point = line.strip().split(" -> ")
+            start_point = list(map(int, point[0].split(",")))
+            end_point = list(map(int, point[1].split(",")))
+            p_start: Point = Point(start_point[0], start_point[1])
+            p_end: Point = Point(end_point[0], end_point[1])
+            l: Line = Line(p_start, p_end)
             if (l.horizontal() or l.vertical()):
                 lines.append(l)
     return lines
 
 
-def points_number_of_two_overlaps(lines: List[Line]) -> int:
-    point_overlaps_number: Dict[Tuple[int, int], int] = {}
+def point_overlaps(lines: List[Line]) -> Dict[Point, int]:
+    point_overlaps_amount: Dict[Point, int] = {}
     for i in range(len(lines)):
-        line_points: List[Tuple[int, int]] = lines[i].get_all_points()
+        line_points: List[Point] = lines[i].get_all_points()
         for point in line_points:
             counter = 1
-            if point in point_overlaps_number:
-                counter += point_overlaps_number.get(point)
-            point_overlaps_number[point] = counter
+            if point in point_overlaps_amount:
+                counter += point_overlaps_amount.get(point)
+            point_overlaps_amount[point] = counter
+    return point_overlaps_amount
 
+
+def points_number_of_two_overlaps(lines: List[Line]) -> int:
+    point_overlaps_amount: Dict[Point, int] = point_overlaps(lines)
     points_number = 0
-    for overlap_number in point_overlaps_number.values():
+    for overlap_number in point_overlaps_amount.values():
         if overlap_number >= 2:
             points_number += 1
     return points_number
-
+    
 
 def main():
     lines: List[Line] = file_read("AdventOfCode2021/day5/day5.txt")
